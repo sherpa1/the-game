f44ac0521f158cce355f0beff0db4c672d1aabd6
sherpa1 <alex@sherpa.one>
2021-01-29 21:27:12 +0100

    up

--- e_gaming_api/utils/MongoDBConnect.js
+++ e_gaming_api/utils/MongoDBConnect.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
-const { DB_NAME, DB_HOST, DB_USER, DB_PWD } = process.env;
+const { DB_NAME, DB_HOST, DB_USER, DB_PWD, DB_PORT } = process.env;
 
-mongoose.connect(`mongodb://${DB_HOST}`, {
-    user: DB_USER,
-    pass: DB_PWD,
-    dbName: DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log(`✅ Successful connection to the NoSQL "${DB_NAME}" MongoDB database`);
-}).catch(error => {
-    throw new Error(error);
-});
+try {
+
+    mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}`, {
+        user: DB_USER,
+        pass: DB_PWD,
+        dbName: DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log(`✅ Successful connection to the NoSQL "${DB_NAME}" MongoDB database`);
+    }).catch(error => {
+        throw new Error(error);
+    });
+} catch (error) {
+    console.error(error);
+}
+
 
 module.exports = mongoose;
